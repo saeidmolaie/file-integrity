1a7da9c4b35983937f091bf9382b59e7729d283a
saeidmolaie <saeedmolaiy.dev@gmail.com>
2026-02-22 13:17:13 +0330

    Implemented MD5 checksum

--- src/Controllers/FileMD5ChecksumController.cs
+++ src/Controllers/FileMD5ChecksumController.cs
@@ -4,6 +4,34 @@ internal class FileMD5ChecksumController : FileChecksumController
 {
 	public override string ResolveChecksum(FileStream stream)
 	{
-		return string.Empty;
+		stream.Seek(0, SeekOrigin.Begin);
+
+		using var md5 = System.Security.Cryptography.MD5.Create();
+
+		int bytesRead;
+		long bytesProcessed = 0;
+
+		var buffer = new byte[8192];
+		var totalBytes = stream.Length;
+
+		while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+		{
+			bytesProcessed += bytesRead;
+
+			if (bytesProcessed == totalBytes)
+			{
+				md5.TransformFinalBlock(buffer, 0, bytesRead);
+			}
+			else
+			{
+				md5.TransformBlock(buffer, 0, bytesRead, buffer, 0);
+			}
+		}
+
+		var hashBytes = md5.Hash;
+
+		return hashBytes == null
+			? throw new Exception("Unable to generate MD5 hash!")
+			: Convert.ToHexStringLower(hashBytes);
 	}
 }
