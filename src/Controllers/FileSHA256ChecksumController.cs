cfe3f43afd4de5aa686cddf5ce476857ea4ad11c
saeidmolaie <saeedmolaiy.dev@gmail.com>
2026-02-23 12:06:12 +0330

    Implemented SHA256 checksum

--- src/Controllers/FileSHA256ChecksumController.cs
+++ src/Controllers/FileSHA256ChecksumController.cs
@@ -1,9 +1,31 @@
-﻿namespace FileIntegrity;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FileIntegrity;
 
 internal class FileSHA256ChecksumController : FileChecksumController
 {
 	public override string ResolveChecksum(FileStream stream)
 	{
-		return string.Empty;
+		ArgumentNullException.ThrowIfNull(stream);
+
+		if (!stream.CanRead)
+			throw new ArgumentException(
+				"Stream must be readable.", nameof(stream));
+
+		if (stream.CanSeek)
+			stream.Position = 0;
+
+		using var sha256 = SHA256.Create();
+		var hashBytes = sha256.ComputeHash(stream);
+
+		var builder = new StringBuilder(hashBytes.Length * 2);
+
+		foreach (var bytes in hashBytes)
+		{
+			builder.Append(bytes.ToString("x2"));
+		}
+
+		return builder.ToString();
 	}
 }
